d52e3d52789f7a402bb3dcf242effaf1ebfe9672
FabrizioCafolla <developer@fabriziocafolla.com>
2025-03-24 21:44:25 +0100

    feat: clean code and add ruff

--- plugins/twinplay/lib/conf/deps.sh
+++ plugins/twinplay/lib/conf/deps.sh
@@ -11,7 +11,7 @@ trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
 set -o pipefail
 
 main(){
-  devbox run deps-install-base
+  devbox run tp:deps:install-base
 
   echo "[INFO] Installing project dependencies"
 }
